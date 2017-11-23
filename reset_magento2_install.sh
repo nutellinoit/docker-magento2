d22ed0448bd5de5ead85dfd3a53b8204fe054c0d
Parallels <parallels@magento2.docker>
2017-11-23 13:52:36 +0100

    Mod compose, mod reset

--- reset_magento2_install.sh
+++ reset_magento2_install.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
 rm -rf magento2
-#cp -r magento2-sample-ce magento2
-unzip magento2-sample-ce/Magento-CE-2.2.1-2017.zip -d magento2
-cd magento2
-#docker run --rm --interactive --tty --volume $PWD:/app nutellinoit/magento2-composer install
+./install_via_composer.sh
